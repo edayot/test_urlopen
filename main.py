5ca45a370e2f491e24e877fb08e7052bdceb3072
Erwan Dayot <35682876+edayot@users.noreply.github.com>
2026-09-08 21:20:25 +0200

    Implement GET request preparation with requests
    Added code to create and prepare a GET request using the requests library, and included a sample URL request using urllib.

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
+import requests
+
+url = 'https://static.wikia.nocookie.net/minecraft_gamepedia/images/f/f8/Lectern_JE2_BE1.png'
+
+# Create a request object without sending it
+req = requests.Request('GET', url, headers={
+    'User-Agent': 'Mozilla/5.0'
+})
+
+# Prepare it to see what will be sent
+prepared = req.prepare()
+
+# Print everything
+print("URL:", prepared.url)
+print("Method:", prepared.method)
+print("Headers:", dict(prepared.headers))
+print("Body:", prepared.body)
+
+
+
+
+
+from urllib.request import urlopen
+
+URL = 'https://static.wikia.nocookie.net/minecraft_gamepedia/images/f/f8/Lectern_JE2_BE1.png'
+
+urlopen(URL)
+
 
