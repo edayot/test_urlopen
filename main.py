327698ed037722e5f9801c8172dfc07ef7cb452a
Erwan Dayot <35682876+edayot@users.noreply.github.com>
2026-09-08 21:29:34 +0200

    Refactor HTTP request handling using urllib

--- main.py
+++ main.py
@@ -1,29 +1,13 @@
-import requests
-
-url = 'https://static.wikia.nocookie.net/minecraft_gamepedia/images/f/f8/Lectern_JE2_BE1.png'
-
-# Create a request object without sending it
-req = requests.Request('GET', url, headers={
-    'User-Agent': 'Mozilla/5.0'
-})
-
-# Prepare it to see what will be sent
-prepared = req.prepare()
-
-# Print everything
-print("URL:", prepared.url)
-print("Method:", prepared.method)
-print("Headers:", dict(prepared.headers))
-print("Body:", prepared.body)
-
-
-
-
-
-from urllib.request import urlopen
+from urllib.request import Request, urlopen
 
 URL = 'https://static.wikia.nocookie.net/minecraft_gamepedia/images/f/f8/Lectern_JE2_BE1.png'
 
-urlopen(URL)
+x = Request(URL)
+print("Method:", x.get_method())
+print("Full URL:", x.full_url)
+print("Headers:", dict(x.headers))
+print("Data:", x.data)
 
+response = urlopen(x)
 
+print(response)
